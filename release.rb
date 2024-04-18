aadc5b4412c3b7a0ef3190566670f721fffb6244
Brett Terpstra <me@brettterpstra.com>
2024-04-18 13:39:03 -0500

    version bump

--- release.rb
+++ release.rb
@@ -9,5 +9,8 @@ new_ver = version.next
 content.sub!(/"version": ".*?"/, "\"version\": \"#{new_ver}\"")
 File.open(manifest, 'w') { |f| f.puts content }
 
+`git commit -a -m "version bump"`
+`git push`
 `git release create -m "v#{new_ver}" #{new_ver}`
+`git pull`
 `gh release upload #{new_ver} main.ts manifest.json`
