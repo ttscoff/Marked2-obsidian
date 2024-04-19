633f4df9b4f7e6bc9e0a0f016626d85208cf6be8
Brett Terpstra <me@brettterpstra.com>
2024-04-19 09:47:01 -0500

    version bump

--- release.rb
+++ release.rb
@@ -1,16 +1,28 @@
 #!/usr/bin/env ruby
 
 manifest = 'manifest.json'
+package = 'package.json'
 
 content = IO.read(manifest)
 version = content.match(/"version": "(\d+\.\d+\.\d+)"/)[1]
 
-new_ver = version.next
+parts = version.match(/(?<maj>\d+)\.(?<min>\d+)\.(?<patch>\d+)/)
+maj = parts['maj']
+min = parts['min']
+patch = parts['patch'].to_i + 1
+
+new_ver = "#{maj}.#{min}.#{patch}"
+
 content.sub!(/"version": ".*?"/, "\"version\": \"#{new_ver}\"")
+
+p_content = IO.read(package)
+p_content.sub!(/"version": ".*?"/, "\"version\": \"#{new_ver}\"")
+
 File.open(manifest, 'w') { |f| f.puts content }
+File.open(package, 'w') { |f| f.puts p_content }
 
 `git commit -a -m "version bump"`
 `git push`
-`gh release create #{new_ver} --generate-notes manifest.json main.ts`
+`gh release create #{new_ver} --generate-notes manifest.json main.js`
 `git pull`
 
